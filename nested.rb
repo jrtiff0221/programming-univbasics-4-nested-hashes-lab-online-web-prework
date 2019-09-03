4c19e0d27800d69a1db1808d25b74414dc14015b
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-03 22:17:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,7 +67,10 @@ def adding_matz
 # }
 
 	programmer_hash = {
-
+ 		  :yukihiro_matsumoto => {
+ 		      :known_for => "Ruby",
+ 		      :languages => ["LISP", "C"]
+ 		    },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -82,7 +85,7 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto]
+    # programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
